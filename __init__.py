d78b11924ef10f84fb377923af706a441dd17127
JunSung Park <bti8coc@gmail.com>
2023-09-13 12:18:59 +0900

    blueprint

--- __init__.py
+++ __init__.py
@@ -14,60 +14,17 @@ def create_app():
     if app.config['DEBUG']: #true일때
         app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1 # 1s로 바꾸니 바로 갱신될것이다.
 
+    ''' Blueprint Route INIT '''
+    from googlekaap.routes import base_route, auth_route
+    app.register_blueprint(base_route.bp)
+    app.register_blueprint(auth_route.bp)
+
+
+
     ''' CSRF INIT '''
     csrf.init_app(app)
 
-
-    @app.route('/')     # 어떤 url에 route시킬것인가를 지정
-    def index():
-        return render_template('index.html')    # 자동으로 상대경로를 templates를 받는다.
-
-    from googlekaap.forms.auth_form import loginForm, RegisterForm
-    @app.route('/auth/login', methods=['GET','POST'])
-    def login():
-        form = loginForm()
-
-        # POST,validate OK
-        if form.validate_on_submit():
-            ## TODO
-            # 1) 유저조회
-            # 2) 유저 이미 존재하는지 체크
-            # 3) 패스워드 정합확인
-            # 4) 로그인유지(세션)
-            user_id = form.data.get('user_id')
-            password = form.data.get('password') 
-            return f'{user_id}, {password}'
-        else:
-            # TODO: Error
-            pass 
-        return render_template('login.html', form=form)
     
-    @app.route('/auth/register',methods=['GET','POST'])
-    def register():
-        form = RegisterForm()
-
-        if form.validate_on_submit():
-            ## TODO
-            # 1) 유저조회
-            # 2) 유저 이미 존재하는지 체크
-            # 3) 없으면 유저 생성
-            # 4) 로그인유지(세션)
-            user_id = form.data.get('user_id')
-            user_name = form.data.get('user_name')
-            password = form.data.get('password') 
-            repassword = form.data.get('repassword')
-            
-            return f'{user_id}, {user_name},{password},{repassword}'
-        else:
-            # TODO: Error
-            pass 
-
-        return render_template('register.html', form=form)
-
-    @app.route('/auth/logout')
-    def logout():
-        return 'logout'
-
     @app.errorhandler(404)
     def page_404(error):
         return render_template('/404.html')
---
+++ routes/auth_route.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, render_template
+from googlekaap.forms.auth_form import loginForm, RegisterForm
+
+NAME = 'auth'
+bp = Blueprint(NAME, __name__, url_prefix='/auth') # blueprint아래에 작성되는 라우터는 모두 prefix를 auth를 갖게된다.공통관리가능. 
+
+@bp.route('/login', methods=['GET','POST'])
+def login():
+    form = loginForm()
+
+    # POST,validate OK
+    if form.validate_on_submit():
+        ## TODO
+        # 1) 유저조회
+        # 2) 유저 이미 존재하는지 체크
+        # 3) 패스워드 정합확인
+        # 4) 로그인유지(세션)
+        user_id = form.data.get('user_id')
+        password = form.data.get('password') 
+        return f'{user_id}, {password}'
+    else:
+        # TODO: Error
+        pass 
+    return render_template(f'{NAME}/login.html', form=form)
+
+@bp.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        ## TODO
+        # 1) 유저조회
+        # 2) 유저 이미 존재하는지 체크
+        # 3) 없으면 유저 생성
+        # 4) 로그인유지(세션)
+        user_id = form.data.get('user_id')
+        user_name = form.data.get('user_name')
+        password = form.data.get('password') 
+        repassword = form.data.get('repassword')
+        
+        return f'{user_id}, {user_name},{password},{repassword}'
+    else:
+        # TODO: Error
+        pass 
+
+    return render_template(f'{NAME}/register.html', form=form)
+
+@bp.route('/logout')
+def logout():
+    return 'logout'
