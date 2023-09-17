cae70b2c590db1c055a4561e3b20bc5dc9b88322
JunSung Park <bti8coc@gmail.com>
2023-09-17 23:00:32 +0900

    refactoring thag G context

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template,g
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,26 +7,28 @@ csrf = CSRFProtect()
 db = SQLAlchemy()
 migrate = Migrate()
 
-
+# flask run -> create_app()
 def create_app():
     print('run: create_app()')
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secretkey'
+    app.config['SECRET_KEY'] = 'secret'
     app.config['SESSION_COOKIE_NAME'] = 'googlekaap'
-    app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:password@localhost/googlekaap?charset=utf8'  ## /googlekaap은 db명을 넣는것이다.
-    app.config['SQLALCHMEY_TACK_MODIFICATIONS'] = False
-    if app.config['DEBUG']: #true일때
-        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1 # 1s로 바꾸니 바로 갱신될것이다.
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/googlekaap?charset=utf8' ## /googlekaap은 db명을 넣는것이다.
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    ''' DB INIT '''
+    if app.config['DEBUG'] == True: #true일때
+        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1 # 1s로 바꾸니 바로 갱신될것이다.
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+    """ === Database Init === """
     db.init_app(app)
     ## TDD할때 sqlite를 사용할건데 altertable이라는 RDBMS의 query중에 테이블의 형상을 바꾸는 쿼리가 안먹을수 있다. sqlite는.왜냐면 test용도니깐. 그런것을 대응해주기 위한 예외처리를 넣어주자.
     if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
-        migrate.init_app(app, db, redner_as_batch=True)
+        migrate.init_app(app, db, render_as_batch=True)
     else:
-        migrate.init_app(app,db)
-    migrate.init_app(app,db)
+        migrate.init_app(app, db)
 
 
     ''' Blueprint Route INIT '''
@@ -37,11 +38,91 @@ def create_app():
 
 
 
-    ''' CSRF INIT '''
+    """ === CSRF Init === """
     csrf.init_app(app)
+
+
+    """ REQUEST HOOK """
+    @app.before_request
+    def before_request():
+        g.db = db.session
+
+    @app.teardown_request
+    def teardown_request(exception):
+        if hasattr(g, 'db'):
+            g.db.close()
 
     
     @app.errorhandler(404)
     def page_404(error):
         return render_template('/404.html')
+    
+    # ''' === Method & Request context Practice === '''
+    # from flask import request
+    # @app.route('/test/method/', defaults={'id': 1}, methods=['GET', 'POST', 'DELETE', 'PUT'])
+    # @app.route('/test/method/<int:id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
+    # def method_test(id):
+        # return jsonify({
+            # 'id': id,
+            # 'request.args': request.args,
+            # 'request.form': request.form,
+            # 'request.json': request.json
+        # })
+
+
+    # ''' === Routing Practice === '''
+    # from flask import jsonify, redirect, url_for
+    # from markupsafe import escape
+
+    # @app.route('/test/name/<name>')
+    # def name(name):
+        # return f'Name is {name}, {escape(type(name))}'
+
+    # @app.route('/test/id/<int:id>')
+    # def id(id):
+        # return 'Id: %d' % id
+
+    # @app.route('/test/path/<path:subpath>')
+    # def path(subpath):
+        # return subpath
+
+    # @app.route('/test/json')
+    # def json():
+        # return jsonify({'hello': 'world'})
+
+    # @app.route('/test/redirect/<path:subpath>')
+    # def redirect_url(subpath):
+        # return redirect(subpath)
+
+    # @app.route('/test/urlfor/<path:subpath>')
+    # def urlfor(subpath):
+        # return redirect(url_for('path', subpath=subpath))
+
+    # ''' === Request hook, Context controll  === '''
+    # # https://flask.palletsprojects.com/en/1.1.x/api/
+    # from flask import g, current_app
+    # @app.before_first_request
+    # def before_first_request():
+        # app.logger.info('BEFORE_FIRST_REQUEST')
+
+    # @app.before_request
+    # def before_request():
+        # g.test = True
+        # app.logger.info('BEFORE_REQUEST')
+
+    # @app.after_request
+    # def after_request(response):
+        # app.logger.info(f"g.test: {g.test}")
+        # app.logger.info(f"current_app.config: {current_app.config}")
+        # app.logger.info("AFTER_REQUEST")
+        # return response
+
+    # @app.teardown_request
+    # def teardown_request(exception):
+        # app.logger.info('TEARDOWN_REQUEST')
+
+    # @app.teardown_appcontext
+    # def teardown_appcontext(exception):
+        # app.logger.info('TEARDOWN_CONTEXT')
+        
     return app
--- models/user.py
+++ models/user.py
@@ -1,9 +1,12 @@
-from googlekaap import db   ## init.py에서 sqlalchemy가 정의되있는거 불러오기
+from googlekaap import db ## init.py에서 sqlalchemy가 정의되있는거 불러오기
 
 ## class는 이제 sql에서 table이 될것이다. 거기서 table명이 User이다.
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(20), unique=True, nullable=False)
     user_name = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    
+    password = db.Column(db.String(200), nullable=False)
+
+    @classmethod 
+    def find_one_by_user_id(cls, user_id):
+        return User.query.filter_by(user_id=user_id).first()
